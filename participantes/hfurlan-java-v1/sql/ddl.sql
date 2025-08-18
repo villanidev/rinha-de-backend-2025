abd6ce5c7e4eb00f914f77d0f22b47c1218e2402
Hildebrando Furlan Neto <hildebrando.furlan@gmail.com>
2025-08-17 23:38:56 -0300

    Correção no endpoint /payments-summary (#3419)
    * Correção no endpoint /payments-summary
    
    * Correção no endpoint /payments-summary

--- participantes/hfurlan-java-v1/sql/ddl.sql
+++ participantes/hfurlan-java-v1/sql/ddl.sql
@@ -8,6 +8,16 @@ create unlogged table payments (
 create index idx_payments_inserted_at on payments(inserted_at);
 
 create unlogged table payment_processor_priority (
-    payment_processor int not null,
+    payment_processor_1_failing char(1) not null,
+    payment_processor_1_min_response_time int not null,
+    payment_processor_1_updated_at timestamp not null,
+    payment_processor_2_failing char(1) not null,
+    payment_processor_2_min_response_time int not null,
+    payment_processor_2_updated_at timestamp not null,
     updated_at timestamp not null
+);
+
+create unlogged table payments_retry (
+    correlation_id char(36) unique primary key,
+    amount decimal not null
 );
